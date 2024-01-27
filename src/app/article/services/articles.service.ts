cbd71dc94b855d6e60174c436ea358ec22b12ba3
DESKTOP-FK5JE54\Albert <albertbabayan94@gmail.com>
2024-01-27 15:50:02 +0400

    small fixes

--- src/app/article/services/articles.service.ts
+++ src/app/article/services/articles.service.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, OnInit } from '@angular/core';
 import { Observable, tap } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { IArticleResponse } from '../interfaces/article-response.interface';
@@ -9,27 +9,24 @@ import { IArticle } from '../interfaces/article.interface';
 
 export class ArticlesService {
 
+  public serverUrl = environment.serverUrl;
   private http = inject(HttpClient);
 
   constructor() {}
 
   public getArticle(payload: number): Observable<any> {
-    const { serverUrl } = environment;
-    return this.http.get<any>(`${serverUrl}/api/article/${payload}`);
+    return this.http.get<any>(`${this.serverUrl}/api/article/${payload}`);
   }
 
   public getArticles(): Observable<IArticleResponse> {
-    const { serverUrl } = environment;
-    return this.http.get<IArticleResponse>(`${serverUrl}/api/articles`);
+    return this.http.get<IArticleResponse>(`${this.serverUrl}/api/articles`);
   }
 
   public updateArticle(payload: Partial<IArticle>, articleId: number): Observable<any> {
-    const { serverUrl } = environment;
-    return this.http.put<any>(`${serverUrl}/api/article/edit/${articleId}`, payload);
+    return this.http.put<any>(`${this.serverUrl}/api/article/edit/${articleId}`, payload);
   }
 
   public createArticle(payload: IArticle): Observable<any> {
-    const { serverUrl } = environment;
-    return this.http.post<any>(`${serverUrl}/api/article/create`, payload);
+    return this.http.post<any>(`${this.serverUrl}/api/article/create`, payload);
   }
 }
